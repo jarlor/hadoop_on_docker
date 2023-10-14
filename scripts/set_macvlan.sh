515c59ad0519ebe3521117dd0cbcf93eb8c4304c
jarlor <zjl58960902@outlook.com>
2023-10-14 21:55:32 +0800

    新增  将容器的IP地址添加到宿主机的路由表中 的脚本

---
+++ scripts/set_macvlan.sh
@@ -0,0 +1,14 @@
+#将容器的IP地址添加到宿主机的路由表中
+
+#!/bin/bash
+#开启网卡混杂模式
+ip link set ${netcard_name} promisc on
+#通过 macvlan 添加一块虚拟网口macvlan0-host来实现容器与宿主机互通
+ip link add link ${netcard_name} macvlan0-host type macvlan mode bridge
+#开启macvlan0-host
+ip link set dev macvlan0-host up
+
+#指定路由
+ip route add ${ip_hadoop1} dev macvlan0-host
+ip route add ${ip_hadoop2} dev macvlan0-host
+ip route add ${ip_hadoop3} dev macvlan0-host
